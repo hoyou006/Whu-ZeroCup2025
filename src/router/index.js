5fcbd18cdbba8045fee89251f5f5348a1cd4d0bc
Jack <zhang2705143462@gmail.com>
2025-11-10 20:59:06 +0800

    小游戏
    initial

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ProvinceView from '@/views/ProvinceView.vue'
 import RouteView from '@/views/RouteView.vue'
+import ChallengeView from '@/views/ChallengeView.vue'
 const routes = [
     { path: '/', name: 'Home', component: HomeView },
     { path: '/province/:name', name: 'Province', component: ProvinceView, props: true },
-    { path: '/RouteView', name: 'RouteView', component: RouteView }
+    { path: '/RouteView', name: 'RouteView', component: RouteView },
+    // 过关闯将 -> 子界面：国家地理知识挑战
+    { path: '/challenge', name: 'Challenge', component: ChallengeView }
 ]
 
 const router = createRouter({
